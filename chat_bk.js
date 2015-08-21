f2c02a0d3cf85a1315428607bf1ddee42ee5ddef
Viegas <felipesviegas@gmail.com>
2015-08-21 10:40:23 -0300

    First Commit

--- chat_bk.js
+++ chat_bk.js
@@ -114,6 +114,7 @@ io.on("connection", function(client) {
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
    var addr = server.address();
    console.log("Chat server listening at", addr.address + ":" + addr.port);
+   console.log(addr.address + ":" + addr.port);
 });
 
 
